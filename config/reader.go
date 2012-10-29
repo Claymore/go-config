1a5609e8bcec337483f275a9c8558e699082a704
Alexey Bobyakov <claymore.ws@gmail.com>
2012-10-29 22:09:55 +0400

    Better API and implementation

--- config/reader.go
+++ config/reader.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strings"
 )
 
 // A ParseError is returned for parsing errors.
@@ -22,32 +23,23 @@ func (e *ParseError) Error() string {
 
 // These are the errors that can be returned in ParseError.Error
 var (
-	ErrParse = errors.New("generic parse error")
+	ErrParse              = errors.New("generic parse error")
+	ErrEmptySectionHeader = errors.New("empty section header")
 )
 
-type Section struct {
-	Name    string
-	Options map[string]string
-}
-
-func NewSection(name string) *Section {
-	return &Section{
-		Name:    name,
-		Options: make(map[string]string),
-	}
-}
-
 type Reader struct {
-	r      *bufio.Reader
-	field  bytes.Buffer
-	line   int
-	column int
+	r              *bufio.Reader
+	field          bytes.Buffer
+	line           int
+	column         int
+	currentSection string
 }
 
 // NewReader returns a new Reader that reads from r.
 func NewReader(r io.Reader) *Reader {
 	return &Reader{
-		r: bufio.NewReader(r),
+		r:              bufio.NewReader(r),
+		currentSection: "default",
 	}
 }
 
@@ -60,37 +52,79 @@ func (r *Reader) error(err error) error {
 	}
 }
 
-func (r *Reader) Read() (section *Section, err error) {
-	for {
-		section, err = r.parseSection()
-		if section != nil {
-			break
-		}
-		if err != nil {
-			return nil, err
+func (r *Reader) readRune() (rune, error) {
+	r1, _, err := r.r.ReadRune()
+
+	// Handle \r\n here.  We make the simplifying assumption that
+	// anytime \r is followed by \n that it can be folded to \n.
+	// We will not detect text which contains both \r\n and bare \n.
+	if r1 == '\r' {
+		r1, _, err = r.r.ReadRune()
+		if err == nil {
+			if r1 != '\n' {
+				r.r.UnreadRune()
+				r1 = '\r'
+			}
 		}
 	}
-
-	return section, nil
+	r.column++
+	return r1, err
 }
 
-func (r *Reader) ReadAll() (sections []Section, err error) {
+// unreadRune puts the last rune read from r back.
+func (r *Reader) unreadRune() {
+	r.r.UnreadRune()
+	r.column--
+}
+
+func (r *Reader) ReadAll() (sections map[string]map[string]string, err error) {
+	sections = make(map[string]map[string]string)
 	for {
-		section, err := r.Read()
+		r.line++
+		r.column = 0
+		r1, err := r.readRune()
 		if err == io.EOF {
 			return sections, nil
-		}
-		if err != nil {
+		} else if err != nil {
 			return nil, err
 		}
-		sections = append(sections, *section)
+
+		switch r1 {
+		case '#', ';':
+			r.skip('\n')
+		case '[':
+			section, err := r.parseHeader()
+			if err != nil {
+				return nil, err
+			}
+			if _, ok := sections[section]; !ok {
+				sections[section] = make(map[string]string)
+			}
+			r.currentSection = section
+		default:
+			r.unreadRune()
+			key, value, err := r.parseOption()
+			if err != nil {
+				return nil, err
+			}
+			key = strings.TrimSpace(key)
+
+			if len(key) != 0 {
+				if r.currentSection == "default" {
+					if _, ok := sections["default"]; !ok {
+						sections["default"] = make(map[string]string)
+					}
+				}
+				sections[r.currentSection][key] = value
+			}
+		}
 	}
 	panic("unreachable")
 }
 
 func (r *Reader) skip(delim rune) error {
 	for {
-		r1, _, err := r.r.ReadRune()
+		r1, err := r.readRune()
 		if err != nil {
 			return err
 		}
@@ -101,88 +135,73 @@ func (r *Reader) skip(delim rune) error {
 	panic("unreachable")
 }
 
-func (r *Reader) parseSection() (section *Section, err error) {
+func (r *Reader) parseHeader() (section string, err error) {
 	r.field.Reset()
-	r1, _, err := r.r.ReadRune()
-	if err != nil {
-		return nil, err
-	}
-	if r1 == '#' {
-		return nil, r.skip('\n')
-	}
+	for {
+		r1, err := r.readRune()
+		if err != nil {
+			return section, err
+		}
 
-	if r1 == '[' {
-		for {
-			r1, _, err := r.r.ReadRune()
-			if err != nil {
-				return nil, err
+		switch r1 {
+		case '#', ';':
+			return section, r.error(ErrParse)
+		case ']':
+			section = r.field.String()
+			if section == "" {
+				return section, r.error(ErrEmptySectionHeader)
 			}
-			if r1 == ']' {
-				section = NewSection(r.field.String())
-				r.skip('\n')
-				break
+			err = r.skip('\n')
+			if err == nil || err == io.EOF {
+				return section, nil
+			} else {
+				return section, err
 			}
+		default:
 			r.field.WriteRune(r1)
 		}
 	}
-
-	if section != nil {
-		for {
-			r1, _, err := r.r.ReadRune()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				return nil, err
-			}
-			r.r.UnreadRune()
-			if r1 == '[' {
-				break
-			}
-			hasOption, key, value, err := r.parseOption()
-			if hasOption {
-				section.Options[key] = value
-			}
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-
-	return section, err
+	panic("unreachable")
 }
 
-func (r *Reader) parseOption() (haveOption bool, key string, value string, err error) {
+func (r *Reader) parseOption() (key string, value string, err error) {
 	r.field.Reset()
-
-	var foundDelim bool
+	var (
+		lastRune   rune
+		foundDelim bool
+	)
 	for {
-		r1, _, err := r.r.ReadRune()
+		r1, err := r.readRune()
 		if err == io.EOF {
 			value = r.field.String()
-			return foundDelim, key, value, nil
+			return key, value, nil
 		}
 		if err != nil {
-			return false, key, value, err
+			return key, value, err
 		}
-		if !foundDelim {
-			if r1 == '=' {
+
+		if (lastRune == 0 || lastRune == ' ') && (r1 == '#' || r1 == ';') {
+			value = r.field.String()
+			return key, value[:len(value)-1], r.skip('\n')
+		}
+
+		switch r1 {
+		case '=':
+			if !foundDelim {
 				key = r.field.String()
 				r.skip(' ')
 				foundDelim = true
 				r.field.Reset()
-				continue
-			} else if r1 != ' ' {
+			} else {
 				r.field.WriteRune(r1)
+				lastRune = r1
 			}
-		} else {
-			if r1 != '\n' {
-				r.field.WriteRune(r1)
-			}
-		}
-		if r1 == '\n' {
+		case '\n':
 			value = r.field.String()
-			return foundDelim, key, value, nil
+			return key, value, nil
+		default:
+			r.field.WriteRune(r1)
+			lastRune = r1
 		}
 	}
 	panic("unreachable")
