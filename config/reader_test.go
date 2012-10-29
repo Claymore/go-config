411da7b1db9b2bb44f441cef396ad47dba375018
Alexey Bobyakov <claymore.ws@gmail.com>
2012-10-29 17:05:24 +0400

    Some more testing

--- config/reader_test.go
+++ config/reader_test.go
@@ -8,10 +8,15 @@ import (
 
 func TestReadAll(t *testing.T) {
     const name = "Some section"
+    const key = "SomeOption"
+    const value = "z = x + y"
     m := new(bytes.Buffer)
-    m.WriteString(fmt.Sprintf("[%s]", name))
+    m.WriteString(fmt.Sprintf("[%s]\n", name))
+    m.WriteString(fmt.Sprintf("%s = %s\n", key, value))
+    m.WriteString("#[Another section]\n")
     reader := NewReader(m)
     sections, err := reader.ReadAll()
+    fmt.Println(sections)
     if err != nil {
         t.Fatal(err)
     }
@@ -20,5 +25,11 @@ func TestReadAll(t *testing.T) {
     }
     if sections[0].Name != name {
         t.Errorf("ReadAll should return section named %q, returned %q", name, sections[0].Name)
+    }
+    if len(sections[0].Options) != 1 {
+        t.Fatalf("ReadAll should return a section with just 1 option, returned %d options", len(sections[0].Options))
+    }
+    if actualValue, ok := sections[0].Options[key]; !ok || actualValue != value  {
+        t.Errorf("ReadAll should return a section with option named %q and value %q, returned value %q", key, value, actualValue)
     }
 }
